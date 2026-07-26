ccda3a4ee72b1845509e0a32464f985f853445d4
Dibyansh <dibyanshfoujdar@gmail.com>
2026-07-26 23:19:02 +0530

    Add files via upload

---
+++ HowMuchDoesDaytonaCost.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HowMuchDoesDaytonaCost {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            boolean found = false;
+
+            for (int i = 0; i < n; i++) {
+                if (sc.nextInt() == k) {
+                    found = true;
+                }
+            }
+
+            System.out.println(found ? "YES" : "NO");
+        }
+
+        sc.close();
+    }
+}
