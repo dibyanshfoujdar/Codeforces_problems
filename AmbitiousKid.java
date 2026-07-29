a15755afc68561ca75d83d203ec7b5d0b7c38151
Dibyansh <dibyanshfoujdar@gmail.com>
2026-07-29 23:42:40 +0530

    Add files via upload

---
+++ AmbitiousKid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class AmbitiousKid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            ans = Math.min(ans, Math.abs(x));
+        }
+
+        System.out.println(ans);
+    }
+}
+
+
