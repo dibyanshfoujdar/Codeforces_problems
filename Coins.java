f2a8b216d2005c8d6cba7f62a64eac20dbca6765
dibyanshfoujdar <dibyanshfoujdar@gmail.com>
2026-08-08 23:22:59 +0530

    1814A Coins

---
+++ Coins.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Coins{
+    public static void main (String [] args){
+         Scanner sc = new Scanner(System.in);
+
+         int t = sc.nextInt();
+
+         while(t-- > 0){
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            if(n % 2 == 0){
+                System.out.println("YES");
+            }else{
+                if(k % 2 != 0){
+                   System.out.println("YES");
+                }else{
+                    System.out.println("NO");
+                }
+            }
+         }
+
+         sc.close();
+    }
+}
